c5c48193ee4a9001b377552b910e285b23d04f25
zar3bski <zarebskidavid@gmail.com>
2025-04-23 16:15:33 +0200

    feat: omv load and set SSL cert

---
+++ roles/openmediavault/files/omv-ssl-update-certs.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+. /usr/share/openmediavault/scripts/helper-functions
+
+cert="/etc/ssl/certs/omv.crt"
+key="/etc/ssl/private/omv.key"
+uuid="757f842e-faf0-11e8-a284-3a6331353066"
+comment="${1}"
+
+if ! omv_isuuid "${uuid}"; then
+    echo "Invalid uuid"
+    exit 1
+fi
+
+if [ ! -f "${cert}" ]; then
+    echo "Cert not found"
+    exit 2
+fi
+
+if [ ! -f "${key}" ]; then
+    echo "Key not found"
+    exit 3
+fi
+
+echo "Cert file :: ${cert}"
+echo "Key file :: ${key}"
+
+xpath="/config/system/certificates/sslcertificate[uuid='${uuid}']"
+echo "xpath :: ${xpath}"
+echo
+
+if ! omv_config_exists "${xpath}"; then
+    echo "Config for ${uuid} does not exist: creating"
+    omv_config_add_node_data "/config/system/certificates" "sslcertificate" \
+        "<uuid>${uuid}</uuid><certificate>${cert}</certificate><privatekey>${key}</privatekey><comment>${comment}</comment>"
+    echo "Config created successfully"
+else
+    echo "Updating certificate in database ..."
+    omv_config_update "${xpath}/certificate" "$(cat ${cert})"
+
+    echo "Updating private key in database ..."
+    omv_config_update "${xpath}/privatekey" "$(cat ${key})"
+
+    echo "Updating comment in database ..."
+    omv_config_update "${xpath}/comment" "${comment}"
+fi
+
+echo "Updating certs and nginx..."
+omv-salt deploy run certificates nginx
+
+systemctl restart nginx
+
+exit 0
